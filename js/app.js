7b5cc33fd51a4fb395432acff41385c5bf7575fe
Corkle <corey.r.mcdaniel@live.com>
2015-09-13 22:13:45 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('myApp', [
+    'ngRoute'
+]);
+
+app.config(function ($routeProvider) {
+    'use strict'
+
+    $routeProvider
+        .when('/inbox', {
+        templateUrl: 'views/inbox.html',
+        controller: 'InboxCtrl',
+    })            
+        .otherwise({
+        redirectTo: 'inbox'
+    });
+});
---
+++ js/controllers/inbox.js
@@ -0,0 +1,8 @@
+angular.module('myApp')
+    .controller('InboxCtrl',
+        function InboxCtrl($scope) {
+            'use strict';
+            $scope.meta = {
+                title: 'My Inbox'
+            };
+        });
